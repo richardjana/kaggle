359ab93d032bb9333fbc65871b8e8363864eca1c
richardjana <richard.c.jana@gmail.com>
2025-03-05 14:15:51 +0200

    improved plotting for category error

--- Playground_Series_5_03--Binary_Prediction_with_a_Rainfall_Dataset/model.py
+++ Playground_Series_5_03--Binary_Prediction_with_a_Rainfall_Dataset/model.py
@@ -2,6 +2,7 @@ import datetime
 import keras
 import matplotlib
 matplotlib.use('Agg')
+import matplotlib.patheffects as PathEffects
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -16,7 +17,7 @@ layer_size = 64
 L2_reg = 0.01 /10
 drop_rate = 0.25
 learning_rate = 0.00001
-epochs = 100
+epochs = 10
 cv_splits = 5
 
 loss_function = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
@@ -27,11 +28,16 @@ stamp = datetime.datetime.timestamp(datetime.datetime.now())
 def clean_data(pd_df):
     pd_df.drop('id', axis=1, inplace=True)
 
+    # replace day with cyclic representation
+    pd_df['day_sin'] = pd_df.apply(lambda row: np.sin(2*np.pi*row.day/2), axis=1)
+    pd_df['day_cos'] = pd_df.apply(lambda row: np.cos(2*np.pi*row.day/2), axis=1)
+    pd_df.drop('day', axis=1, inplace=True)
+
     return pd_df
 
 ##### load data,  split into train / validation / test #####
 dataframe = clean_data(pd.read_csv('train.csv'))
-dataframe, rest = train_test_split(dataframe, test_size=0.80) # reduce dataset size for testing
+#dataframe, rest = train_test_split(dataframe, test_size=0.80) # reduce dataset size for testing
 train, val = train_test_split(dataframe, test_size=0.2)
 
 def make_new_model(shape):
@@ -74,14 +80,35 @@ def make_prediction(model, i):
     test['rainfall'] = prediction
     test.to_csv(f"predictions_KFold_{i}.csv", columns=['id', 'rainfall'], index=False)
 
-####### cross-validation code #######
+def make_category_error_plot(pd_df, i, name, n=2):
+    h_map = np.zeros((n, n))
+    counts = pd_df[['id', 'rainfall', 'PREDICTION']].groupby(['rainfall', 'PREDICTION'], as_index=False).count().values.tolist()
+    for r, p, c in counts:
+        h_map[p, r] = c
+
+    h_map /= np.sum(h_map)
+
+    cmap = sns.color_palette('rocket', as_cmap=True)
+    chart = sns.heatmap(h_map, cmap=cmap, square=True, linewidths=.5, cbar_kws={'shrink': .5})
+
+    for y in range(n):
+        for x in range(n):
+            txt = plt.text(x + 0.5, y + 0.5, f"{h_map[y, x]:.3f}", ha='center', va='center')
+            txt.set_path_effects([PathEffects.withStroke(linewidth=3, foreground='w'), PathEffects.withStroke(linewidth=1, foreground='k')])
+
+    chart.invert_yaxis()
+    chart.set_xlabel('Label')
+    chart.set_ylabel('Predicted label')
+
+    plt.savefig(f"category_error_{i}_{name}.png", bbox_inches='tight')
+    plt.close()
+
 kfold = KFold(n_splits=cv_splits, shuffle=True)
 scores = []
 
 y_train = dataframe.pop('rainfall').to_numpy()
 X_train = dataframe.to_numpy()
 
-#for train_index, val_index in kfold.split(X_train):
 i = 0
 for train_index, val_index in kfold.split(X_train, y_train):
     X_train_fold, X_val_fold = X_train[train_index], X_train[val_index]
@@ -94,41 +121,18 @@ for train_index, val_index in kfold.split(X_train, y_train):
 
     model.save(f"rainfall_KFold_{i}.keras")
     make_training_plot(history.history, i)
+
+    df_train = pd.DataFrame({'rainfall': y_train_fold, 'id': y_train_fold})
+    df_train['PREDICTION'] = np.argmax(tf.nn.softmax(model.predict(X_train_fold)), axis=1).reshape(-1,)
+    df_val = pd.DataFrame({'rainfall': y_val_fold, 'id': y_val_fold})
+    df_val['PREDICTION'] = np.argmax(tf.nn.softmax(model.predict(X_val_fold)), axis=1).reshape(-1,)
+    make_category_error_plot(df_train, i, 'training', n=2)
+    make_category_error_plot(df_val, i, 'validation', n=2)
+
     make_prediction(model, i)
+
     i += 1
     scores.append(history.history[f"val_{metric}"][-1])
 
 avg_score = np.mean(scores) # Calculate the average cross-validation score
 print(f'Average cross-validation score: {avg_score:.4f} ({scores})')
-####### end cross-validation code #######
-
-##### make diagonal error plot #####
-def accuracy(a, b):
-    return np.sum(a==b)/len(a)
-
-#train['PREDICTION'] = np.argmax(tf.nn.softmax(model.predict(df_to_dataset(train, shuffle=False))), axis=1).reshape(-1,)
-#val['PREDICTION'] = np.argmax(tf.nn.softmax(model.predict(df_to_dataset(val, shuffle=False))), axis=1).reshape(-1,)
-
-# replace with different plot
-def make_diagonal_plot(train, val):
-    chart = sns.scatterplot(data=train, x='rainfall', y='PREDICTION', alpha=0.25)
-    sns.scatterplot(data=val, x='rainfall', y='PREDICTION', alpha=0.25)
-
-    min_val = min(chart.get_xlim()[0], chart.get_ylim()[0])
-    max_val = max(chart.get_xlim()[1], chart.get_ylim()[1])
-    chart.set_xlim([min_val, max_val])
-    chart.set_ylim([min_val, max_val])
-    chart.plot([min_val, max_val], [min_val, max_val], linewidth=1, color='k')
-
-    chart.set_aspect('equal')
-    chart.set_xlabel('Label')
-    chart.set_ylabel('Predicted label')
-
-    labels = [f"training ({accuracy(train['label'], train['PREDICTION']):.2f})"]
-    labels += [f"validation ({accuracy(val['label'], val['PREDICTION']):.2f})"]
-    plt.legend(labels=labels, title='dataset (accuracy):', loc='best')
-
-    plt.savefig(f"error_{stamp}.png", bbox_inches='tight')
-    plt.close()
-
-#make_diagonal_plot(train, val)
