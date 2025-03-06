b80cc7baff0150be705782f8bb59b52426c9b411
richardjana <richard.c.jana@gmail.com>
2025-03-06 15:02:40 +0200

    test regression model

---
+++ Playground_Series_5_03--Binary_Prediction_with_a_Rainfall_Dataset/regression.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+from scipy.optimize import minimize
+import seaborn as sns
+import sklearn
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+import sys
+sys.path.append('../')
+from kaggle_utilities import make_category_error_plot
+
+target_col = 'rainfall'
+
+def clean_data(pd_df): # clean dataset
+    pd_df.drop('id', axis=1, inplace=True)
+
+    pd_df.rename(columns={target_col: 'TARGET'}, inplace=True)
+
+    # replace day with cyclic representation
+    pd_df['day_sin'] = pd_df.apply(lambda row: np.sin(2*np.pi*row.day/2), axis=1)
+    pd_df['day_cos'] = pd_df.apply(lambda row: np.cos(2*np.pi*row.day/2), axis=1)
+    pd_df.drop('day', axis=1, inplace=True)
+
+    return pd_df
+
+##### load data,  split into train / validation / test #####
+dataframe = clean_data(pd.read_csv('train.csv'))
+dataframe, rest = train_test_split(dataframe, test_size=0.80) # reduce dataset size for testing
+train, val = train_test_split(dataframe, test_size=0.2)
+test = clean_data(pd.read_csv('test.csv'))
+
+def fit_linear_model(X, y):
+    model = LogisticRegression()
+    model.fit(X, y)
+
+    return model
+
+def x_y_pd_dataframe(pd_df):
+    y = pd_df['TARGET'].to_numpy()
+    x_df = pd_df.drop('TARGET', axis=1)
+    X = x_df.to_numpy()
+
+    return X, y
+
+# train linear model
+X, y = x_y_pd_dataframe(train)
+model = fit_linear_model(X, y)
+
+# make predictions on train / test set
+train['PREDICTION'] = model.predict(X)
+print("Training accuracy:", accuracy_score(train['TARGET'], train['PREDICTION']))
+print('Training F1:', f1_score(train['TARGET'], train['PREDICTION']))
+X, y = x_y_pd_dataframe(val)
+val['PREDICTION'] = model.predict(X)
+print("Validation accuracy:", accuracy_score(val['TARGET'], val['PREDICTION']))
+print('Validation F1:', f1_score(val['TARGET'], val['PREDICTION']))
+
+train.rename(columns={'pressure': 'id'}, inplace=True)
+val.rename(columns={'pressure': 'id'}, inplace=True)
+make_category_error_plot(train, 'training')
+make_category_error_plot(val, 'validation')
