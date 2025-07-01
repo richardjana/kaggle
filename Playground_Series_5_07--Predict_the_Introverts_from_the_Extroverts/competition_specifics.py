63d625da5021d3f20c14d35a12bd1b20528e73e5
richardjana <richard.c.jana@gmail.com>
2025-07-01 13:08:41 +0300

    base scripts

---
+++ Playground_Series_5_07--Predict_the_Introverts_from_the_Extroverts/competition_specifics.py
@@ -0,0 +1,90 @@
+from itertools import combinations
+from typing import Dict, Tuple
+
+import numpy as np
+import pandas as pd
+from scipy.stats import zscore
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+
+
+TARGET_COL = 'Personality'
+COMPETITION_NAME = 'playground-series-s5e7'
+
+
+def clean_data(pd_df: pd.DataFrame) -> pd.DataFrame:
+    """ Apply cleaning operations to pandas DataFrame.
+    Args:
+        pd_df (pd.DataFrame): The DataFrame to clean.
+        drop (bool, optional): If rows should be dropped (option for training DataFrame) or not
+            (test DataFrame). Defaults to True.
+    Returns:
+        pd.DataFrame: Cleaned DataFrame.
+    """
+    try:
+        pd_df.drop('id', axis=1, inplace=True)
+    except KeyError:
+        pass
+
+    for cat_col in ['Stage_fear', 'Drained_after_socializing']:
+        pd_df[cat_col] = pd_df[cat_col].fillna('unknown').astype('category')
+
+    for num_col in ['Time_spent_Alone',  'Social_event_attendance', 'Going_outside',
+                    'Friends_circle_size', 'Post_frequency']:
+        # .astype('category')#
+        pd_df[num_col] = pd_df[num_col].fillna(pd_df[num_col].median()).astype('int')
+
+    return pd_df
+
+
+def add_derived_cols(df: pd.DataFrame) -> pd.DataFrame:
+    """ Engineered features, as recommended by Komil Parmar.
+    Args:
+        df (pd.DataFrame): A DataFrame ...
+    Returns:
+        pd.DataFrame: ... with the added columns.
+    """
+    def is_temp_suitable(row: pd.Series) -> int:
+        crop_temp_map = {'Barley': (15, 25),
+                         'Cotton': (25, 35),
+                         'Ground Nuts': (25, 32),
+                         'Maize': (25, 32),
+                         'Millets': (25, 35),
+                         'Oil seeds': (20, 30),
+                         'Paddy': (25, 35),
+                         'Pulses': (20, 30),
+                         'Sugarcane': (26, 35),
+                         'Tobacco': (20, 30),
+                         'Wheat': (20, 30)
+                         }
+
+        min_temp, max_temp = crop_temp_map[row['Crop Type']]
+
+        return 1 if min_temp <= row['Temperature'] <= max_temp else 0
+
+    scaler = MinMaxScaler()
+    env_cols = ['Temperature', 'Humidity', 'Moisture']
+    df[env_cols] = scaler.fit_transform(df[env_cols])
+
+    df['env_max'] = df[env_cols].max(axis=1)
+    df['temp_suitability'] = df.apply(is_temp_suitable, axis=1)
+
+    return df
+
+
+def load_preprocess_data() -> Tuple[pd.DataFrame, pd.DataFrame, LabelEncoder]:
+    """ Prepare training and test data into pandas DataFrames: added columns, transformations, etc.
+    Returns:
+        Tuple[pd.DataFrame, pd.DataFrame]: Training, test and original data, ready for model
+            training.
+    """
+    train = clean_data(pd.read_csv('train.csv'))
+    test = clean_data(pd.read_csv('test.csv'))
+
+    # constant feature to add variety to tree ensemble
+    #train['constant'] = pd.Categorical(['const'] * len(train))
+    #test['constant'] = pd.Categorical(['const'] * len(test))
+
+    label_encoder = LabelEncoder()
+    train[TARGET_COL] = label_encoder.fit_transform(train[TARGET_COL])
+
+    return train, test, label_encoder
---
+++ Playground_Series_5_07--Predict_the_Introverts_from_the_Extroverts/xgb_model_optuna.py
@@ -0,0 +1,146 @@
+import sys
+from typing import List
+
+import joblib
+import numpy as np
+from numpy.typing import NDArray
+import optuna
+import pandas as pd
+from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import LabelEncoder
+import xgboost as xgb
+
+sys.path.append('/'.join(__file__.split('/')[:-2]))
+from competition_specifics import TARGET_COL, COMPETITION_NAME, load_preprocess_data
+from kaggle_api_functions import submit_prediction
+from kaggle_utilities import plot_confusion_matrix
+
+try:
+    SERIAL_NUMBER = sys.argv[1]
+except IndexError:
+    SERIAL_NUMBER = 0
+
+
+def make_prediction(test_fold_preds: List[NDArray], encoder: LabelEncoder) -> None:
+    """ Make a prediction for the test data, with a given model and ecoder for the target.
+    Args:
+        test_fold_preds (List[NDArray]): Predictions for each fold.
+        encoder (LabelEncoder): Label encoder to recover the correct labels.
+    """
+    submit_df = pd.read_csv('sample_submission.csv')
+    submit_df[TARGET_COL] = np.median(np.array(test_fold_preds), axis=0).astype(int)
+    submit_df[TARGET_COL] = encoder.inverse_transform(submit_df[TARGET_COL])
+    submit_df.to_csv('predictions_XGB_optuna.csv',
+                     columns=['id', TARGET_COL], index=False)
+
+
+# Load dataset
+train, test, encoder = load_preprocess_data()
+
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+ADDITIONAL_PARAMS = {'early_stopping_rounds': 100,
+                     'enable_categorical': True,
+                     'eval_metric': 'logloss',
+                     'n_estimators': 10_000,
+                     'objective': 'binary:logistic',
+                     'random_state': 77,
+                     'tree_method': 'hist',
+                     'use_label_encoder': False,
+                     'verbosity': 0,
+                     }
+
+
+def objective(trial):
+    """ Objective function for Optuna. """
+    params = {'colsample_bytree': trial.suggest_float('colsample_bytree', 0.3, 1.0),
+              'gamma': trial.suggest_float('gamma', 1e-3, 10, log=True),
+              'learning_rate': trial.suggest_float('learning_rate', 1e-3, 0.1, log=True),
+              'max_delta_step': trial.suggest_float('max_delta_step', 1e-3, 10, log=True),
+              'max_depth': trial.suggest_int('max_depth', 3, 20),
+              'min_child_weight': trial.suggest_int('min_child_weight', 1, 100),
+              'subsample': trial.suggest_float('subsample', 0.5, 1.0),
+              'reg_alpha': trial.suggest_float('reg_alpha', 1e-3, 10.0, log=True),
+              'reg_lambda': trial.suggest_float('reg_lambda', 1e-3, 10.0, log=True)
+              }
+    params.update(ADDITIONAL_PARAMS)
+
+    oof_preds = np.zeros(len(train))
+
+    for train_idx, val_idx in skf.split(train, train[TARGET_COL]):
+        X_train_fold, X_val_fold = train.iloc[train_idx].copy(), train.iloc[val_idx].copy()
+
+        y_train_fold = X_train_fold.pop(TARGET_COL)
+        y_val_fold = X_val_fold.pop(TARGET_COL)
+
+        model = xgb.XGBClassifier(**params)
+        model.fit(X_train_fold, y_train_fold,
+                  eval_set=[(X_val_fold, y_val_fold)],
+                  verbose=False
+                  )
+
+        oof_preds[val_idx] = model.predict(X_val_fold)
+
+    # consider scoring trials by other metric:
+    #print(f"accuracy = {accuracy_score(train[TARGET_COL], oof_preds)}")
+    #print(f"auc = {roc_auc_score(train[TARGET_COL], oof_probas[:, 1])}")
+
+    return accuracy_score(oof_preds, train[TARGET_COL])
+
+# Create and optimize Optuna study
+study = optuna.create_study(direction='maximize')
+study.optimize(objective, n_trials=10, timeout=60*60*5)
+
+best_params = study.best_params
+best_params.update(ADDITIONAL_PARAMS)
+
+# Print best trial
+print(f"Best trial: {study.best_trial.number}/{len(study.trials)}")
+print(f"  accuracy: {study.best_value}")
+print(f"  worst accuracy: {min(trial.value for trial in study.trials if trial.value is not None)}")
+print('  Best hyperparameters:')
+for key, value in best_params.items():
+    print(f"    {key}: {value}")
+
+
+# Train final model with best parameters
+oof_preds = np.zeros(len(train))
+oof_probas = np.zeros((len(train), 2))
+test_fold_preds = []
+
+for train_idx, val_idx in skf.split(train, train[TARGET_COL]):
+    X_train_fold, X_val_fold = train.iloc[train_idx].copy(), train.iloc[val_idx].copy()
+
+    y_train_fold = X_train_fold.pop(TARGET_COL)
+    y_val_fold = X_val_fold.pop(TARGET_COL)
+
+    model = xgb.XGBClassifier(**best_params)
+    model.fit(X_train_fold, y_train_fold,
+              eval_set=[(X_val_fold, y_val_fold)],
+              verbose=False
+              )
+
+    oof_preds[val_idx] = model.predict(X_val_fold)
+    oof_probas[val_idx, :] = model.predict_proba(X_val_fold)
+
+    test_fold_preds.append(model.predict(test))
+
+
+# save predictions for ensembling
+joblib.dump({'oof_preds': oof_preds,
+             'test_preds': np.mean(test_fold_preds, axis=0),
+             'y_train': train[TARGET_COL].values
+             },
+             'stacking_data.pkl')
+
+# make prediction for the test data
+make_prediction(test_fold_preds, encoder)
+
+plot_confusion_matrix(train[TARGET_COL].to_numpy(), oof_preds.astype(int),
+                      'confusion_matrix.png', encoder.classes_)
+exit()
+final_accuracy = accuracy_score(train[TARGET_COL], oof_preds)
+public_score = submit_prediction(COMPETITION_NAME, 'predictions_XGB_optuna.csv',
+                                 f"XGB optuna {SERIAL_NUMBER} categories ({final_accuracy})")
+print(f"Public score: {public_score}")
